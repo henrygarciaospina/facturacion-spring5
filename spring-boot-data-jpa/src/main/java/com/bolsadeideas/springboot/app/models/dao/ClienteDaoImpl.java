7b49e1bde8bc89f5ead90876c08bb0c41b277c9b
hgarciaospina <31645964+hgarciaospina@users.noreply.github.com>
2018-11-12 00:17:43 -0500

    Sección 4, clase 32 Creando controlador con la acción handler listar

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.app.models.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,7 @@ import com.bolsadeideas.springboot.app.models.entity.Cliente;
 @Repository
 public class ClienteDaoImpl implements IClienteDao {
 
+	@PersistenceContext
 	private EntityManager em;
 	
 	@SuppressWarnings("unchecked")
